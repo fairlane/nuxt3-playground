38a8f1c5d5f374bba864dff98da7ede4325a9a02
Hannu Pölönen <hannu.polonen@diktamen.com>
2022-02-03 09:52:21 +0200

    Minor styling fixes

--- components/Layout.vue
+++ components/Layout.vue
@@ -1,13 +1,13 @@
 <template>
-<div  class="grid grid-cols-2 gap-2 flex items-center m-20 border border-gray-200 rounded shadow p-3">
-  <div>
+<div  class="grid grid-cols-2 gap-2 flex items-start m-20 border border-gray-200 rounded shadow p-3">
+  <div class="top-20 left-20">
       <signup />
   </div>
-  <div>
-      <users />
-  </div>
-  <div>
+  <div class="top-20 right-20">
     <search-users />
+  </div>
+  <div class="top-20 right-20">
+      <users />
   </div>
 </div>
 </template>
